d4a013c5a604894d88a786b5595dbaf62ab7eac9
charles <arianepaulabarros@gmail.com>
2022-10-31 12:10:30 -0300

    initial commit

---
+++ spider.py
@@ -0,0 +1,34 @@
+import scrapy
+import json
+
+
+def urls_brands():
+    base_url = 'https://www.rankingthebrands.com/The-Brands-and-their-Rankings.aspx?catFilter=0&nameFilter='
+    alphabet = 'ABCDEFGHIJKLMNO'
+    urls = list()
+    for c in alphabet:
+        urls.append(base_url + c)
+    return urls
+
+
+def write_results(brands):
+    sorted_brands = sorted(brands, key=lambda d: d['brand_name'])
+    jsonstring = json.dumps(sorted_brands)
+    output_file = open('marcas.json', 'w')
+    output_file.write(jsonstring)
+    output_file.close()
+
+class BrandsSpider(scrapy.Spider):
+    name = 'brands'
+    start_urls = urls_brands()
+    # esta sera a lista de marcas depois do agente ter feito o trabalho.
+    brands = list()
+
+    def parse(self, response):
+        for e in response.css('.rankingName'):
+            brand_to_write = e.css('::text').get()
+            self.brands.append({'name': brand_to_write})
+            yield {'name': brand_to_write}
+
+    def close(self, reason):
+        write_results(self.brands)
